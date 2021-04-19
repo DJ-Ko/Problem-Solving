f606d7652ec688b11b29fd32ba691d00ece51587
kni56 <kni56@121.166.186.20>
2021-04-19 16:14:36 +0900

    update

--- SWQT/src/q16236.java
+++ SWQT/src/q16236.java
@@ -8,7 +8,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Solution {
+public class q16236 {
 	
 	static int[][] map;
 //	static int[][] visit;
